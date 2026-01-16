143028668f5fc23923fb04935d833915467eef58
Aryan3572 <157047439+Aryan3572@users.noreply.github.com>
2026-01-16 12:23:24 +0530

    added mongodb to my todo application

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/data",  function(req,res) {
+app.post("/todo", async function(req,res) {
     const createPayload = req.body;
     const parsePayload = createTodo.safeParse(createPayload);
     if(!parsePayload.success) {
@@ -13,13 +13,26 @@ app.post("/data",  function(req,res) {
         })
         return;
     }
+    await todo.Create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed: false
+    })
+    res.json({
+        msg: "Todo created"
+    })
 })
 
-app.get("/data",  function(req,res) {
+app.get("/todos", async function(req,res) {
+    const todos = await todo.find();
+    // console.log(todos); // promises
 
+    res.json({
+        todos
+    })
 })
 
-app.put("/data", function(req,res) {
+app.put("/completed", async function(req,res) {
     const updatePayload = req.body;
     const parsePayload= updateTodo.safeParse(updatePayload);
     if(!parsePayload.success) {
@@ -28,4 +41,13 @@ app.put("/data", function(req,res) {
         })
         return;
     }
+    await todo.update({
+        _id: req.body.id 
+    }, {
+            completed: true
+        
+    })
+    res.json({
+        msg: "Todo maked as completed"
+    })
 })
